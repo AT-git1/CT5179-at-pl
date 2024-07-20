3c5bee6487842b13c0b40e86180f8a0225f565cf
Patrick Loftus <DonLofto@users.noreply.github.com>
2024-07-20 15:02:58 +0100

    - added comments for better understanding of view and middleware usage.

--- client/app.js
+++ client/app.js
@@ -12,17 +12,18 @@ const faqsRouter = require('./routes/faqs');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Set up view engine
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+app.set('view engine', 'ejs'); // Set EJS as the view engine
 
-// Use express-ejs-layouts
+// Use express-ejs-layouts for layout support
 app.use(expressLayouts);
-app.set('layout', 'layout'); // Default layout
+app.set('layout', 'layout'); // Set the default layout
 
 // Use morgan for HTTP request logging
 app.use(morgan('dev', {
   skip: function (req, res) {
+    // Skip logging for successful requests and static assets
     return res.statusCode < 400
         || req.url.includes('/bootstrap/')
         || req.url.includes('/jquery/')
@@ -30,33 +31,44 @@ app.use(morgan('dev', {
   },
   stream: {
     write: function (message) {
+      // Log messages using winston
       winstonLogger.info(message.trim());
     }
   }
 }));
 
+// Middleware to parse JSON and URL data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Serve static files before setting up the routes
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/popper', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
 
+// Set up routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/faqs', faqsRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const createError = require('http-errors');
   next(createError(404));
 });
 
-// error handler
+// Error handler middleware
+/**
+ * Middleware function to handle errors.
+ *
+ * @param {Error} err - The error object.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const errorHandler = require('./middleware/errorHandler');
 app.use(errorHandler);
 
